ac2d767fd9862cd9611977ad48217a63dc81734e
BrianNeisen <brianjneisen@gmail.com>
2023-10-04 01:09:08 -0500

    Some basic edits

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,29 @@
 
-const Questions =  ["Whats a variable?", "Whats a Function", "Whats a Array"];
+// Setting up an array of questions to be asked
+const QuestionList = ["Whats a variable?", "Whats a Function", "Whats a Array"];
+
+// sets the intial question when opening browser
+let questionNumber = document.getElementById("get").innerHTML = QuestionList[0]
+
+
+
+
+const Quiz = () => {  
+
+
+    
+    
+
+
+
+
+
+
+
+}
+
+
+Quiz();
 
 
 
