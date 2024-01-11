f8a252cc46eeb00115a073f577f5eb42b14a37fb
Briam <boomyoman@gmail.com>
2024-01-11 15:55:33 -0600

    creating quizes

---
+++ frontend/quizzes/arrayQuiz.js
@@ -0,0 +1,28 @@
+const arrayQuestions = [
+    { 
+      question: "What is a  variable?",
+      answer: [
+        { text: "A type of button", isCorrect: false },
+        { text: " a place to store data", isCorrect: true },
+        { text: "idk", isCorrect: false },
+      ],
+    },
+    {
+      question: "What is a Array?",
+      answer: [
+        { text: "A variable that stores a list of values ", isCorrect: true },
+        { text: " A type of object ", isCorrect: false },
+        { text: "idk", isCorrect: false },
+      ],
+    },
+  
+    {
+      question: "What is a function?",
+      answer: [
+        { text: " A type of class  ", isCorrect: false },
+        { text: " A set of statements that perform a job", isCorrect: true },
+        { text: "idk", isCorrect: false },
+      ],
+    },
+  ]; 
+  
