0c7bab1660f3330d7f11e6127964bbd7fac7f6c3
Briam <boomyoman@gmail.com>
2024-03-15 20:35:24 -0500

    creating backend

---
+++ backend/storeData.js
@@ -0,0 +1,18 @@
+const sqlite3  = require(sqlite3).verbose(); 
+
+const db = new sqlite3.Database('./database.db',sqlite3.OPEN_READWRITE,(err)=> { 
+
+    if(err) { 
+
+        return console.error(err.message);
+
+    }
+
+    else { 
+
+        console.log("Connection Successful")
+
+    }
+
+
+})
