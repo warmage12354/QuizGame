80bfb462c679516f43e02ca2c1d033de38efe188
Briam <boomyoman@gmail.com>
2024-03-15 21:07:23 -0500

    More database features

--- backend/storeData.js
+++ backend/storeData.js
@@ -1,18 +1,25 @@
-const sqlite3  = require(sqlite3).verbose(); 
+// set up variables
+const sqlite3 = require('sqlite3').verbose(); 
+let sql;
 
+// connect database
 const db = new sqlite3.Database('./database.db',sqlite3.OPEN_READWRITE,(err)=> { 
 
-    if(err) { 
-
-        return console.error(err.message);
-
-    }
-
-    else { 
-
-        console.log("Connection Successful")
-
-    }
+    if(err)  return console.error(err.message);
+}) 
 
 
+ // create table       
+sql = 'Create TABLE Quiz(totalNumberCorrect int,totalNumberWrong int ,Average int ) ' 
+db.run(sql)
+
+//Drop Table
+db.run("DROP TABLE users") 
+
+
+//insert data into table  
+
+sql = 'Insert INTO Quiz(totalNumberCorrect,totalNumberWrong,Average) VALUES (?,?,?)' 
+db.run(sql,[],(err)=> { 
+    if(err) return console.error(err.message)
 })
